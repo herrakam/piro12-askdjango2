24428ba047cdaa176634bba518b23830fc465373
정승현 <fm1003@naver.com>
2020-01-22 22:06:30 +0900

    8

--- accounts/views.py
+++ accounts/views.py
@@ -25,6 +25,8 @@ class SignupView(CreateView):
 
     def get_success_url(self):
         return resolve_url('profile')
+        next_url = self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
 
     def form_valid(self, form):
         user = form.save()
