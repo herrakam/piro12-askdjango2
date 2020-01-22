845854d332eb00e9ff198766de9910da5a5c9824
정승현 <fm1003@naver.com>
2020-01-22 18:19:48 +0900

    5

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
 
 @login_required
 def profile(request):
     request.user
     return render(request, 'accounts/profile.html')
+
+signup = CreateView.as_view(model=User,form_class=UserCreationForm,success_url=settings.LOGIN_URL,template_name='accounts/signup.html')
