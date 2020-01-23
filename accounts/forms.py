b1a36799a266aeeb6f399c14aac03eb42e8ef7d9
정승현 <fm1003@naver.com>
2020-01-23 10:48:50 +0900

    9

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm
-
+from django.core.validators import validate_email
 
 class SignupForm(UserCreationForm):
     #UserCreationForm에 내용을 오버라이딩해 추가하고싶다면 여기에 쓸것
+    def clean_username(self):
+        value = self.cleaned_data.get('username')
+        if value:
+            validate_email(value)
+        return value
     pass
